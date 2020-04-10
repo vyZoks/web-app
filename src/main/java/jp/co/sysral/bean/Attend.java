84616c179a90399ff65cd8244c157e0fb2b928be
Kazuma Shiigai <km2s84a4@yahoo.co.jp>
2020-04-10 17:58:54 +0900

    Create new EmployeeBean and AttendBean

---
+++ src/main/java/jp/co/sysral/bean/Attend.java
@@ -0,0 +1,65 @@
+package jp.co.sysral.bean;
+
+import java.sql.Timestamp;
+
+public class Attend {
+
+	private int atendId;
+	private Timestamp punchIn;
+	private Timestamp punchOut;
+	private String realBreakTime;
+	private String operatingTime;
+	private String location;
+	private boolean atendFlag;
+	private int empId;
+	
+	public int getAtendId() {
+		return atendId;
+	}
+	public void setAtendId(int atendId) {
+		this.atendId = atendId;
+	}
+	public Timestamp getPunchIn() {
+		return punchIn;
+	}
+	public void setPunchIn(Timestamp punchIn) {
+		this.punchIn = punchIn;
+	}
+	public Timestamp getPunchOut() {
+		return punchOut;
+	}
+	public void setPunchOut(Timestamp punchOut) {
+		this.punchOut = punchOut;
+	}
+	public String getRealBreakTime() {
+		return realBreakTime;
+	}
+	public void setRealBreakTime(String realBreakTime) {
+		this.realBreakTime = realBreakTime;
+	}
+	public String getOperatingTime() {
+		return operatingTime;
+	}
+	public void setOperatingTime(String operatingTime) {
+		this.operatingTime = operatingTime;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public boolean isAtendFlag() {
+		return atendFlag;
+	}
+	public void setAtendFlag(boolean atendFlag) {
+		this.atendFlag = atendFlag;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	
+}
---
+++ src/main/java/jp/co/sysral/bean/Employee.java
@@ -0,0 +1,72 @@
+package jp.co.sysral.bean;
+
+import java.sql.Time;
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private String mailaddress;
+	private String empPass;
+	private Time startWork;
+	private Time endWork;
+	private String unitTime;
+	private String breakTime;
+	private String accessId;
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getMailaddress() {
+		return mailaddress;
+	}
+	public void setMailaddress(String mailaddress) {
+		this.mailaddress = mailaddress;
+	}
+	public String getEmpPass() {
+		return empPass;
+	}
+	public void setEmpPass(String empPass) {
+		this.empPass = empPass;
+	}
+	public Time getStartWork() {
+		return startWork;
+	}
+	public void setStartWork(Time startWork) {
+		this.startWork = startWork;
+	}
+	public Time getEndWork() {
+		return endWork;
+	}
+	public void setEndWork(Time endWork) {
+		this.endWork = endWork;
+	}
+	public String getUnitTime() {
+		return unitTime;
+	}
+	public void setUnitTime(String unitTime) {
+		this.unitTime = unitTime;
+	}
+	public String getBreakTime() {
+		return breakTime;
+	}
+	public void setBreakTime(String breakTime) {
+		this.breakTime = breakTime;
+	}
+	public String getAccessId() {
+		return accessId;
+	}
+	public void setAccessId(String accessId) {
+		this.accessId = accessId;
+	}
+	
+}
