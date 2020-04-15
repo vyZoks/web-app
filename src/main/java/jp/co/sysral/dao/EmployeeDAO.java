1f9a70b455b77418195fdf0411e08b62eed8c59c
Kazuma Shiigai <km2s84a4@yahoo.co.jp>
2020-04-15 19:28:41 +0900

    Added the function to update attendance status

--- src/main/java/jp/co/sysral/dao/EmployeeDAO.java
+++ src/main/java/jp/co/sysral/dao/EmployeeDAO.java
@@ -8,6 +8,7 @@ import jp.co.sysral.bean.Employee;
 
 public class EmployeeDAO extends DAO {
 
+	// １件検索
 	public Employee search(String mailaddress, String password) throws Exception {
 		
 		Employee emp = new Employee();
@@ -37,5 +38,22 @@ public class EmployeeDAO extends DAO {
 		co.close();
 		
 		return emp;
+	}
+	
+	// １件更新(出退勤情報)
+	public void update(boolean atendFlag, int empId) throws Exception {
+		
+		Connection co = getConnection();
+		
+		String sql = "UPDATE s_employee SET atend_flag = ? WHERE emp_id = ?";
+		PreparedStatement ps = co.prepareStatement(sql);
+		ps.setBoolean(1, atendFlag);
+		ps.setInt(2, empId);
+		int cnt = ps.executeUpdate();
+		
+		System.out.println(cnt + "件のデータを更新しました。");
+		
+		ps.close();
+		co.close();
 	}
 }
