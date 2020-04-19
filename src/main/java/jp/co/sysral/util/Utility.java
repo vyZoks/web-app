39029bda09ec63324ea2aa18e399e6469afa99b8
Kazuma Shiigai <km2s84a4@yahoo.co.jp>
2020-04-19 17:27:29 +0900

    Create a new function to calculate attendance time and a new test class

--- src/main/java/jp/co/sysral/util/Utility.java
+++ src/main/java/jp/co/sysral/util/Utility.java
@@ -1,19 +1,89 @@
 package jp.co.sysral.util;
 
+import java.sql.Time;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.Objects;
 
 public class Utility {
 
-	public static Timestamp conv(String dateTime) {
+	// String型をTimestamp型に変換する
+	public static Timestamp conv(String date) {
 		
 		try {
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-			Timestamp ts = new Timestamp(sdf.parse(dateTime).getTime());
+			Timestamp ts = new Timestamp(sdf.parse(date).getTime());
 			return ts;
 		} catch(Exception e) {
 			e.printStackTrace();
 			return null;
 		}
+	}
+	
+	// Timestamp型から日付をString型で返却する
+	public static String getDate(Timestamp date) {
+		
+		if(Objects.isNull(date)) return null;
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd");
+		return sdf.format(date);
+	}
+	
+	// Timestamp型から時間をString型で返却する
+	public static String getTime(Timestamp date) {
+		
+		if(Objects.isNull(date)) return null;
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+		return sdf.format(date);
+	}
+	
+	// 出勤時間と退勤時間から稼働時間を算出する
+	public static String getOperatingHours(Timestamp attendance, Timestamp leaving, Time opening, Time closing, String credit, String rest) {
+		
+		// 実出勤時間または実退勤時間がnullの場合
+		if(Objects.isNull(attendance) || Objects.isNull(leaving)) return null;
+		// 出勤時間または退勤時間がnullの場合
+		if(Objects.isNull(opening) || Objects.isNull(closing)) return null;
+		// 単位時間がnullまたはブランクの場合
+		if(Objects.isNull(credit) || credit.isEmpty()) return null;
+		// 休憩時間がnullまたはブランクの場合
+		if(Objects.isNull(rest) || rest.isEmpty()) return null;
+		
+		Long millisecond = 60L * 60L * 1000L;
+		Long attendanceMillisec = operationHoursAdjustment(attendance, opening, credit);
+		Long leavingMillisec = operationHoursAdjustment(leaving, closing, credit);
+		// 退勤時間(ミリ秒) - 出勤時間(ミリ秒) - 休憩時間(ミリ秒)
+		Long operatingHours = leavingMillisec - attendanceMillisec - (Long.parseLong(rest) * 60L * 1000L);
+		
+		// 稼働時間(ミリ秒) / 1時間(ミリ秒) = 稼働時間(時間)
+		return String.valueOf(operatingHours.doubleValue() / millisecond.doubleValue());
+	}
+	
+	// 出勤時間または退勤時間から単位時間外の時間を調整する
+	public static Long operationHoursAdjustment(Timestamp date, Time time, String credit) {
+		
+		// 開始時刻(終了時刻)の差分(ミリ秒)を算出
+		Long diff = time.getTime() - date.getTime();
+		System.out.println("diff:" + diff);
+		
+		// ミリ秒(差分)を分に変換
+		Long min = diff / (60L * 1000L);
+		System.out.println("min:" + min);
+		
+		// 分 % 単位時間(分)で剰余(分)を算出
+		Long rem = min % Long.parseLong(credit);
+		System.out.println("rem:" + rem);
+		
+		// 分(剰余)をミリ秒に変換
+		Long remMin = rem * (60L * 1000L);
+		System.out.println("remMin:" + remMin);
+		
+		// 剰余が０の場合は実稼働時間(ミリ秒)をそのまま返却
+		if (rem == 0) return date.getTime();
+		// 剰余が０でない場合は実稼働時間から剰余を加減算したものを返却
+		if (rem != 0) return date.getTime() + remMin;
+		
+		return 0L;
 	}
 }
---
+++ src/test/java/jp/co/sysral/util/UtilityTest.java
@@ -0,0 +1,46 @@
+package jp.co.sysral.util;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class UtilityTest {
+
+	@Test
+	@Ignore
+	public void test001() throws Exception {
+		
+		String credit = "5";
+		String rest = "60";
+		Time opening = new Time(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2020-04-18 09:00:00").getTime());
+		Time closing = new Time(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2020-04-18 18:00:00").getTime());
+		Timestamp attendance = new Timestamp(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2020-04-18 09:00:00").getTime());
+		Timestamp leaving = new Timestamp(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2020-04-18 18:00:00").getTime());
+		
+		String operatingHours = Utility.getOperatingHours(attendance, leaving, opening, closing, credit, rest);
+		
+		assertThat(operatingHours, equalTo("8.0"));
+	}
+	
+	@Test
+	public void test002() throws Exception {
+		
+		String credit = "5";
+		String rest = "60";
+		Time opening = new Time(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("1970-01-01 09:00:00").getTime());
+		Time closing = new Time(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("1970-01-01 18:00:00").getTime());
+		Timestamp attendance = new Timestamp(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2020-04-18 09:00:00").getTime());
+		Timestamp leaving = new Timestamp(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2020-04-18 18:02:00").getTime());
+		
+		String operatingHours = Utility.getOperatingHours(attendance, leaving, opening, closing, credit, rest);
+		
+		assertThat(operatingHours, equalTo("8.0"));
+	}
+
+}
