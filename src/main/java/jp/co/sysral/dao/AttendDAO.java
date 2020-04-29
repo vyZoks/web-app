7058ca3b47e26d38f3f682ca2c320ebb33d45005
Kazuma Shiigai <km2s84a4@yahoo.co.jp>
2020-04-29 10:07:48 +0900

    Addition of registration, update and deletion functions Method newAttendanceInfoAddition are deleted and unified as a registered method Method attendanceInfoUpdate, leavingInfoUpdate are deleted and unified as update method

--- src/main/java/jp/co/sysral/dao/AttendDAO.java
+++ src/main/java/jp/co/sysral/dao/AttendDAO.java
@@ -3,7 +3,6 @@ package jp.co.sysral.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +11,7 @@ import jp.co.sysral.bean.Attend;
 public class AttendDAO extends DAO {
 	
 	// 全件検索
-	public List<Attend> search(int empId) throws Exception {
+	public List<Attend> allSearch(int empId) throws Exception {
 		
 		List<Attend> list = new ArrayList<>();
 		
@@ -70,60 +69,35 @@ public class AttendDAO extends DAO {
 		return att;
 	}
 	
-	// 出勤情報新規追加
-	public void newAttendanceInfoAddition(Timestamp date, int empId) throws Exception {
+	// １件検索
+	public Attend search(int attendId) throws Exception {
+		
+		Attend att = new Attend();
 		
 		Connection co = getConnection();
 		
-		String sql = "INSERT INTO s_attend(attendance_time, emp_id) VALUES (?, ?)";
+		String sql = "SELECT * FROM s_attend WHERE attend_id = ?";
 		PreparedStatement ps = co.prepareStatement(sql);
-		ps.setTimestamp(1, date);
-		ps.setInt(2, empId);
-		int cnt = ps.executeUpdate();
+		ps.setInt(1, attendId);
+		ResultSet rs = ps.executeQuery();
 		
-		System.out.println("出勤情報" + cnt + "件を登録しました。");
+		while (rs.next()) {
+			att.setAttendId(rs.getInt("attend_id"));
+			att.setAttendanceTime(rs.getTimestamp("attendance_time"));
+			att.setLeavingTime(rs.getTimestamp("leaving_time"));
+			att.setActualRestTime(rs.getString("actual_rest_time"));
+			att.setOperatingHours(rs.getString("operating_hours"));
+			att.setLocation(rs.getString("location"));
+			att.setEmpId(rs.getInt("emp_id"));
+		}
 		
 		ps.close();
 		co.close();
+		
+		return att;
 	}
 	
-	// 出勤情報更新
-	public void attendanceInfoUpdate(Timestamp date, int attendId) throws Exception {
-		
-		Connection co = getConnection();
-		String sql = "UPDATE s_attend SET attendance_time = ? WHERE attend_id = ?";
-		
-		PreparedStatement ps = co.prepareStatement(sql);
-		ps.setTimestamp(1, date);
-		ps.setInt(2, attendId);
-		int cnt = ps.executeUpdate();
-		
-		System.out.println("出勤情報" + cnt + "件を更新しました。");
-		
-		ps.close();
-		co.close();
-	}
-	
-	// 退勤情報更新
-	public void leavingInfoUpdate(Timestamp date, String actualRestTime, String operatingHours, int attendId) throws Exception {
-		
-		Connection co = getConnection();
-		String sql = "UPDATE s_attend SET leaving_time = ?, actual_rest_time = ?, operating_hours = ? WHERE attend_id = ?";
-		
-		PreparedStatement ps = co.prepareStatement(sql);
-		ps.setTimestamp(1, date);
-		ps.setString(2, actualRestTime);
-		ps.setString(3, operatingHours);
-		ps.setInt(4, attendId);
-		int cnt = ps.executeUpdate();
-		
-		System.out.println("退勤情報" + cnt + "件を更新しました。");
-		
-		ps.close();
-		co.close();
-	}
-	
-	// シーケンス(attenId)の現在値を取得する処理
+	// シーケンス(attenId)の現在値を取得
 	public int searchAttendId(int empId) throws Exception {
 		
 		Connection co = getConnection();
@@ -143,5 +117,55 @@ public class AttendDAO extends DAO {
 		co.close();
 		
 		return seq;
+	}
+	
+	// 登録
+	public void insert(Attend attend) throws Exception {
+		
+		Connection co = getConnection();
+		
+		String sql = "INSERT INTO s_attend(attendance_time, emp_id) VALUES (?, ?)";
+		PreparedStatement ps = co.prepareStatement(sql);
+		ps.setTimestamp(1, attend.getAttendanceTime());
+		ps.setInt(2, attend.getEmpId());
+		int cnt = ps.executeUpdate();
+		
+		System.out.println("出退勤情報" + cnt + "件を登録しました。");
+		ps.close();
+		co.close();
+	}
+	
+	// 更新
+	public void update(Attend attend) throws Exception {
+		
+		Connection co = getConnection();
+		
+		String sql = "UPDATE s_attend SET attendance_time = ?, leaving_time = ?, actual_rest_time = ?, operating_hours = ? WHERE attend_id = ?";
+		PreparedStatement ps = co.prepareStatement(sql);
+		ps.setTimestamp(1, attend.getAttendanceTime());
+		ps.setTimestamp(2, attend.getLeavingTime());
+		ps.setString(3, attend.getActualRestTime());
+		ps.setString(4, attend.getOperatingHours());
+		ps.setInt(5, attend.getAttendId());
+		int cnt = ps.executeUpdate();
+		
+		System.out.println("出退勤情報" + cnt + "件を更新しました。");
+		ps.close();
+		co.close();
+	}
+	
+	// 削除
+	public void delete(int attendId) throws Exception {
+		
+		Connection co = getConnection();
+		
+		String sql = "DELETE FROM s_attend WHERE attend_id = ?";
+		PreparedStatement ps = co.prepareStatement(sql);
+		ps.setInt(1, attendId);
+		int cnt = ps.executeUpdate();
+		
+		System.out.println("出退勤情報" + cnt + "件を削除しました。");
+		ps.close();
+		co.close();
 	}
 }
